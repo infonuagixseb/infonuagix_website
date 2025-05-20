18105094dcff880abde6495145f9d3cfc846543a
Sébastien de Grandpré <sebastien.degrandpre@infonuagix.com>
2025-05-20 22:49:37 +0000

    Error: Couldn't find next-intl config file. Please follow the instructions at https://next-intl.dev/docs/getting-started/app-router

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,3 +1,4 @@
+
 import {getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 
@@ -9,6 +10,6 @@ export default getRequestConfig(async ({locale}) => {
   if (!locales.includes(locale as any)) notFound();
 
   return {
-    messages: (await import(`./messages/${locale}.json`)).default
+    messages: (await import(`@/messages/${locale}.json`)).default
   };
 });
